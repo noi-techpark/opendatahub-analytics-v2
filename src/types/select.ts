725bd072b9c1cae422c3c3eb8c78050f0efc22f4
luho <luca.pasquarelli.villa@gmail.com>
2024-12-03 11:41:48 +0100

    reuse compliant

--- src/types/select.ts
+++ src/types/select.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 export interface SelectOption<T = string | number | undefined> {
    label: string
    value: T
