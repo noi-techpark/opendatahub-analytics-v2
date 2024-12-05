647b1d3ba86c1a7a172cf8203e69f016955072f0
Alessio Crea <alessio.crea98@gmail.com>
2024-12-05 18:26:32 +0100

    fix: missing port env

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+console.log(process.env.VITE_SERVER_PORT)
+
 // https://vitejs.dev/config/
-export default defineConfig({
-   plugins: [vue()],
-   server: {
-      port: 3000,
-   },
-   resolve: {
-      alias: {
-         '@': fileURLToPath(new URL('./src', import.meta.url)),
+
+export default defineConfig(({ command, mode }) => {
+   // Load env file based on `mode` in the current working directory.
+   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+   const env = loadEnv(mode, process.cwd(), '')
+
+   return {
+      plugins: [vue()],
+      server: {
+         port: env.VITE_SERVER_PORT ? Number(env.VITE_SERVER_PORT) : 3000,
       },
-   },
+      resolve: {
+         alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+         },
+      },
+   }
 })
